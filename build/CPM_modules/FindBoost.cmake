7d26d3d2d57544d0f647b46f69da230d016e6af8
Boris Shatskikh <boris190903@gmail.com>
2025-04-07 11:24:25 +0300

    Удалил служебные папки

--- build/CPM_modules/FindBoost.cmake
+++
@@ -1,3 +0,0 @@
-include("C:/Users/HP/Cpp_projects/LDLT_server/build/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("NAME;Boost;VERSION;1.87.0;URL;https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.gz;OPTIONS;BOOST_ENABLE_CMAKE ON;BOOST_INCLUDE_LIBRARIES multiprecision")
-set(Boost_FOUND TRUE)
--- build/CPM_modules/Findasio.cmake
+++
@@ -1,3 +0,0 @@
-include("C:/Users/HP/Cpp_projects/LDLT_server/build/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;chriskohlhoff/asio;GIT_TAG;asio-1-32-0;VERSION;1.32.0;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(asio_FOUND TRUE)
